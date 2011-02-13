284fc8cc28584f053df8aaf0d22f282fddf66d82
Yeri Tiete <yeri@tiete.be>
2011-02-13 22:16:53 +0100

    namebench

--- bind/generatelist.sh
+++ bind/generatelist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Yeri Tiete 
+# Yeri Tiete
 #	aka Tuinslak
 # http://yeri.be
 
@@ -10,8 +10,8 @@ OUTPUT=list.txt
 # delete old file if existing
 [ -e $OUTPUT ] && rm $OUTPUT
 
-# get 6th column,		get unique	remove arpa requests
-awk '{print $6 }' $FILE | awk '!x[$0]++' | awk '{print tolower($0)}' | grep -Ev 'in-addr.arpa' > $TMPFILE
+# get 6th column,		get unique	tolower()		remove arpa requests	remove random namebench queries
+awk '{print $6 }' $FILE | awk '!x[$0]++' | awk '{print tolower($0)}' | grep -Ev 'in-addr.arpa' | grep -Ev 'namebench' > $TMPFILE
 
 while read a; do {
 	[[ `wget $a --tries=1 --user-agent="URL-Extractor (http://yeri.be/ij)" --no-check-certificate -O /dev/null 2>&1 | grep "200 OK"` != "" ]] && echo $a >> $OUTPUT
