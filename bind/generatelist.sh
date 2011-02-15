4be36a8781b14417029d2b304324191871e5757c
Yeri Tiete <yeri@tiete.be>
2011-02-16 00:59:13 +0100

    added output, lowered timeout

--- bind/generatelist.sh
+++ bind/generatelist.sh
@@ -6,13 +6,26 @@
 FILE=example.log
 TMPFILE=/tmp/_urlextractor
 OUTPUT=list.txt
+i=0
 
+echo "	> Starting URL generator at `date -u +%H:%M:%S`."
 # delete old file if existing
+echo "	>> Cleaning up $OUTPUT."
 [ -e $OUTPUT ] && rm $OUTPUT
 
+echo "	>> generating $TMPFILE."
 # get 6th column,		tolower()		get unique   remove '<', '>', '=', '#'	remove arpa requests	remove random namebench queries		remove more namebench crap
-awk '{print $6 }' $FILE | awk '{print tolower($0)}' | awk '!x[$0]++' | sed '/[=#<>]/d' | awk '{print tolower($0)}' | grep -Ev 'in-addr.arpa' | grep -Ev 'namebench' | egrep -Ev '[a-z0-9]{26}' | sort > $TMPFILE
+awk '{print $6 }' $FILE | awk '{print tolower($0)}' | awk '!x[$0]++' | sed '/[=#<>]/d' | awk '{print tolower($0)}' | grep -Ev 'in-addr.arpa' | grep -Ev 'ip6.arpa' | grep -Ev 'namebench' | egrep -Ev '[a-z0-9]{26}' | sort > $TMPFILE
 
+echo "	>> Starting HTTP checker at `date -u +%H:%M:%S`."
+echo "	>>> Please be patient."
 while read a; do {
-	[[ `wget $a --timeout=30 --tries=1 --user-agent="URL-Extractor (http://yeri.be/ij)" --no-check-certificate -O /dev/null 2>&1 | grep "200 OK"` != "" ]] && echo $a >> $OUTPUT
+	let i++
+	echo "	>>> ($i) $a at `date -u +%H:%M:%S`."
+	[[ `wget $a --timeout=3 --tries=1 --user-agent="URL-Extractor (http://yeri.be/ij)" --no-check-certificate -O /dev/null 2>&1 | grep "200 OK"` != "" ]] && echo $a >> $OUTPUT && echo "	>>>> 	ok" || echo "	>>>> 	nok"
 } done < $TMPFILE
+
+# done
+echo "	>>> Done, $i URLs checked."
+echo "	>> $OUTPUT generated."
+echo "	> End of program."
