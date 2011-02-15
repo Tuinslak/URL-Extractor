0216df4c6ef266820950a1e45254f138c71bd1bb
Yeri Tiete <yeri@tiete.be>
2011-02-15 12:13:07 +0100

    clean namebench stuff & weird chars

--- bind/generatelist.sh
+++ bind/generatelist.sh
@@ -10,8 +10,8 @@ OUTPUT=list.txt
 # delete old file if existing
 [ -e $OUTPUT ] && rm $OUTPUT
 
-# get 6th column,		tolower()		get unique	remove arpa requests	remove random namebench queries
-awk '{print $6 }' $FILE | awk '{print tolower($0)}' | awk '!x[$0]++' | awk '{print tolower($0)}' | grep -Ev 'in-addr.arpa' | grep -Ev 'namebench' | sort > $TMPFILE
+# get 6th column,		tolower()		get unique   remove '<', '>', '=', '#'	remove arpa requests	remove random namebench queries		remove more namebench crap
+awk '{print $6 }' $FILE | awk '{print tolower($0)}' | awk '!x[$0]++' | sed '/[=#<>]/d' | awk '{print tolower($0)}' | grep -Ev 'in-addr.arpa' | grep -Ev 'namebench' | egrep -Ev '[a-z0-9]{26}' | sort > $TMPFILE
 
 while read a; do {
 	[[ `wget $a --timeout=30 --tries=1 --user-agent="URL-Extractor (http://yeri.be/ij)" --no-check-certificate -O /dev/null 2>&1 | grep "200 OK"` != "" ]] && echo $a >> $OUTPUT
